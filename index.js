4067026aa82fc21771a487a5f994ffc3555c4a3a
Alpesh Savaliya <118974099+savaliyaalpesh@users.noreply.github.com>
2023-06-03 11:39:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const mainContainer = document.querySelector(".main-container")
+const thankContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("submit")
+const rateAgain = document.getElementById("rate-again")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+submitButton.addEventListener("click", () => {
+    thankContainer.classList.remove("hidden")
+    mainContainer.style.display = "none"
+})
+
+rateAgain.addEventListener("click", () => {
+    thankContainer.classList.add("hidden")
+    mainContainer.style.display = "block"
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
